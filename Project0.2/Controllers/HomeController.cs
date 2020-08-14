132532b5bfb8d00a6f64cd220de1c8a8975e8dfd
Harrison Hess <harrisonhess1@aol.com>
2020-08-14 17:44:55 -0400

    rerouted admin index and list pages

--- Project0.2/Controllers/HomeController.cs
+++ Project0.2/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Project0._2.Models;
@@ -26,6 +27,7 @@ namespace Project0._2.Controllers
         {
             return View();
         }
+        [Authorize]
         public IActionResult AdminIndex()
         {
             return View();
@@ -35,18 +37,19 @@ namespace Project0._2.Controllers
         {
             return View();
         }
-
+        [Authorize]
         public IActionResult VolunteerList()
         {
             ViewData.Model = _volunteerRepository.getVolunteers();
             return View();
         }
-
+        [Authorize]
         public IActionResult OpportunityList()
         {
             ViewData.Model = _opportunityRepository.GetOpportunities();
             return View();
         }
+        [Authorize]
         public IActionResult DeleteOpportunity(int id)
         {
             _opportunityRepository.DeleteOpportunity(id);
@@ -59,23 +62,27 @@ namespace Project0._2.Controllers
             return View();
         }
         [HttpGet]
+        [Authorize]
         public IActionResult EditOpportunity(int id)
         {
             ViewData.Model = _opportunityRepository.GetOpportunity(id);
             return View();
         }
         [HttpPost]
+        [Authorize]
         public IActionResult SaveOpportunity(Opportunity opportunity)
         {
             _opportunityRepository.SaveOpportunity(opportunity);
             return RedirectToAction("OpportunityList");
         }
         [HttpPost]
+        [Authorize]
         public IActionResult CreateOpportunity(Opportunity opportunity)
         {
             _opportunityRepository.CreateOpportunity(opportunity);
             return RedirectToAction("OpportunityList");
         }
+        [Authorize]
         public IActionResult CreateOpportunity()
         {
             return View();
@@ -87,29 +94,33 @@ namespace Project0._2.Controllers
             return View();
         }
         [HttpGet]
+        [Authorize]
         public IActionResult EditVolunteer(int id)
         {
             ViewData.Model = _volunteerRepository.getVolunteer(id);
             return View();
         }
         [HttpPost]
+        [Authorize]
         public IActionResult SaveVolunteer(Volunteer volunteer)
         {
             _volunteerRepository.SaveVolunteer(volunteer);
             return RedirectToAction("VolunteerList");
         }
-
+        [Authorize]
         public IActionResult DeleteVolunteer(int id)
         {
             _volunteerRepository.DeleteVolunteer(id);
             return RedirectToAction("VolunteerList");
         }
         [HttpPost]
+        [Authorize]
         public IActionResult CreateVolunteer(Volunteer volunteer)
         {
             _volunteerRepository.createVolunteer(volunteer);
             return RedirectToAction("VolunteerList");
         }
+        [Authorize]
         public IActionResult CreateVolunteer()
         {
             return View();
